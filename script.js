7cb0b925d668eae6db453416b9e2296ebbdb790c
Syed Abdul Aziz <92917995+syedazeez256@users.noreply.github.com>
2022-07-25 17:12:22 +0500

    Hoisting and prototyping and lexical scoping

---
+++ script.js
@@ -0,0 +1,84 @@
+'use strict';
+
+// function calcAge(birthyear) {
+//   const age = 2037 - birthyear;
+//   function printAge() {
+//     const output = `${firstname}, You are the ${age} years old and born in ${birthyear}`;
+//     console.log(output);
+//     if (birthyear >= 1981 && birthyear <= 2001) {
+//       const firstname = 'Ahmad';
+//       var millenial = true;
+//       const str = `Oh, you are a millenial, ${firstname}`;
+//       console.log(str);
+//       function add(a, b) {
+//         return a + b;
+//       }
+//     }
+//     console.log(millenial);
+//     console.log(add(2, 3));
+//   }
+//   printAge();
+//   return age;
+// }
+
+// const firstname = 'Azeez';
+// calcAge(1999);
+
+// console.log(azzy);
+
+// var azzy = function azzy() {
+//   console.log('Name');
+// };
+
+// if (!numProducts) deleteShoppingCard();
+// var numProducts = 10;
+
+// function deleteShoppingCard() {
+//   console.log('All items are deleted ');
+// }
+
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+// console.log(x === window.x);
+
+// const p = document.querySelector('.p');
+// setTimeout(() => {
+//   p.textContent = 'My name is Azeez';
+// }, 5000);
+// p.style.color = 'red';
+
+// for (var i = 0; i <= 10; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   }, 1000);
+// }
+
+// function Obj(givenName) {
+//   this.name = givenName;
+// }
+// Obj.prototype.getName = function () {
+//   return this.name;
+// };
+
+// Obj.prototype.setName = function (newName) {
+//   return (this.name = newName);
+// };
+
+// const obj2 = new Obj('Azeez');
+
+// console.log(obj2.setName('Hello'));
+
+let arr = ['aziz', 'yasir', 'qasim'];
+let obj = {
+  name: 'Azzy',
+  age: 23,
+  city: 'Lahore',
+  getName: function () {
+    console.log('Print name' + this.name + 'here is age ' + this.name);
+  },
+};
+function func() {
+  //
+}
